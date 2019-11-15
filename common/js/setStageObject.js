f1e5717c4db7db01e04d52fc743e3281012cc957
KentaIshikawa <k.ishikawa0914@gmail.com>
2019-11-15 21:32:53 +0900

    更新

--- common/js/setStageObject.js
+++ common/js/setStageObject.js
@@ -3,27 +3,27 @@
 ////////////////////////////////////////
 var Rec = new Class();
 Rec.extend({
-	"type":"",
-	"startTime":"",
-	"endTime":"",
-	"totalPoint":0,
-	"point":5,
-	"addPoint":function(){
+	"type": "",
+	"startTime": "",
+	"endTime": "",
+	"totalPoint": 0,
+	"point": 5,
+	"addPoint": function () {
 		var st = this.startTime;
 		var en = this.endTime;
-		var re = en-st;
-		if(re>=3000 && re<8000){
-			this.point = this.point-(Math.floor(re/1000)-3);
-		}else if(re>=8000){
+		var re = en - st;
+		if (re >= 3000 && re < 8000) {
+			this.point = this.point - (Math.floor(re / 1000) - 3);
+		} else if (re >= 8000) {
 			this.point = 0;
 		}
 		this.totalPoint += this.point
 		this.init();
 	},
-	"init":function(){
-		this.startTime="";
-		this.endTime="";
-		this.point=5;
+	"init": function () {
+		this.startTime = "";
+		this.endTime = "";
+		this.point = 5;
 	}
 });
 
@@ -32,22 +32,22 @@ Rec.extend({
 ////////////////////////////////////////
 var seane1 = new Seane();
 seane1.addClass("seane1");
-seane1.set(function(){
+seane1.set(function () {
 	var obTitle = new Ob();
 	obTitle.addClass("title");
 	obTitle.html = "<span class='html'>HTML</span> <span class='and'>&</span> <span class='css'>CSS</span>";
-	
+
 	seane1.addChild(obTitle);
-	
+
 	var obSubTitle = new Ob();
 	obSubTitle.addClass("subTitle");
 	obSubTitle.html = Func.sAllWrapSpan("タイピングゲーム");
 	seane1.addChild(obSubTitle);
-	
+
 	var btn = new Btn();
 	btn.html = "<span>START</span>";
 	btn.addClass("btnStart");
-	btn.addEvent(btn.id,"click",function(){
+	btn.addEvent(btn.id, "click", function () {
 		Stage.removeStage(seane1);
 		Stage.addStage(seane2);
 	});
@@ -63,37 +63,37 @@ seane1.set(function(){
 ////////////////////////////////////////
 var seane2 = new Seane();
 seane2.addClass("seane2");
-seane2.added=function(){
-$(window).on("keydown.start",function(e){
-	if(e.keyCode == 32){
-		Stage.removeStage(seane2);
-		Stage.addStage(seane3);
-	}
-	return false;
-});
+seane2.added = function () {
+	$(window).on("keydown.start", function (e) {
+		if (e.keyCode == 32) {
+			Stage.removeStage(seane2);
+			Stage.addStage(seane3);
+		}
+		return false;
+	});
 }
-seane2.removed=function(){
+seane2.removed = function () {
 	$(window).off(".start");
 }
-seane2.set(function(){
+seane2.set(function () {
 	var ob1 = new Ob();
 	ob1.addClass("ob1");
 	ob1.html = "<i></i>キーボードを使います。<br>日本語入力は解除し、直接入力にして下さい。";
 	seane2.addChild(ob1);
-	
+
 	var ob2 = new Ob();
 	ob2.addClass("ob2");
-	ob2.html ="HTMLタグ、HTML属性<br>CSSプロパティ、CSSの値から<br>よく使う物が、ランダムに20単語出題されます。<br>ローマ字入力でタイピングして下さい。";
+	ob2.html = "HTMLタグ、HTML属性<br>CSSプロパティ、CSSの値から<br>よく使う物が、ランダムに20単語出題されます。<br>ローマ字入力でタイピングして下さい。";
 	seane2.addChild(ob2);
-	
+
 	var ob3 = new Ob();
 	ob3.addClass("ob3");
-	ob3.html ="スペースキーを押すと始まります";
+	ob3.html = "スペースキーを押すと始まります";
 	seane2.addChild(ob3);
-	
+
 	var btn = new Btn();
 	btn.html = "<span>タイトルへ戻る</span>";
-	btn.addEvent(btn.id,"click",function(){
+	btn.addEvent(btn.id, "click", function () {
 		Stage.removeStage(seane2);
 		Stage.addStage(seane1);
 	});
@@ -112,13 +112,13 @@ seane2.set(function(){
 ////////////////////////////////////////
 var seane3 = new Seane();
 seane3.addClass("seane3");
-seane3.added = function(){
+seane3.added = function () {
 	$(".ob1 span").hide();
-	$(".ob1 span:nth-child(1)").fadeIn(1000,function(){
+	$(".ob1 span:nth-child(1)").fadeIn(1000, function () {
 		$(this).hide();
-		$(".ob1 span:nth-child(2)").fadeIn(1000,function(){
+		$(".ob1 span:nth-child(2)").fadeIn(1000, function () {
 			$(this).hide();
-			$(".ob1 span:nth-child(3)").fadeIn(1000,function(){
+			$(".ob1 span:nth-child(3)").fadeIn(1000, function () {
 				$(this).hide();
 				Stage.removeStage(seane3);
 				Stage.addStage(seane4);
@@ -126,7 +126,7 @@ seane3.added = function(){
 		});
 	});
 };
-seane3.set(function(){
+seane3.set(function () {
 	var ob1 = new Ob();
 	ob1.addClass("ob1");
 	ob1.html = Func.sAllWrapSpan("321");
@@ -141,37 +141,37 @@ seane3.set(function(){
 ////////////////////////////////////////
 var seane4 = new Seane();
 seane4.addClass("seane4");
-seane4.removed=function(){
+seane4.removed = function () {
 	$(window).off();
 }
 
 
-seane4.set (function(){
+seane4.set(function () {
 	var ob1 = new Ob();
 	ob1.addClass("ob1");
-	ob1.addEvent(ob1.id,"type",function(){
-		ob1.html = "残り"+typing.qText.length+"問です";
-		$("#"+ob1.id).html(ob1.html);
+	ob1.addEvent(ob1.id, "type", function () {
+		ob1.html = "残り" + typing.qText.length + "問です";
+		$("#" + ob1.id).html(ob1.html);
 	});
 	ob1.html = "残り20問です";
 	seane4.addChild(ob1);
 
 	var typing = new TypingGameObject();
-	typing.added = function(){
+	typing.added = function () {
 		typing.wordInit("all");
 		typing.setWord();
-		typing.callBackSelector= ob1.id
-		$(window).keydown(function(e){
+		typing.callBackSelector = ob1.id
+		$(window).keydown(function (e) {
 			typing.typing(e);
 		});
-		$(window).on("over",function(){
+		$(window).on("over", function () {
 			seane4.removeChild(ob1.id);
-			setTimeout(function(){
-				$("#"+seane4.id).fadeOut(1000,function(){
+			setTimeout(function () {
+				$("#" + seane4.id).fadeOut(1000, function () {
 					Stage.removeStage(seane4);
 					Stage.addStage(seane5);
-				})	
-			},1500);
+				})
+			}, 1500);
 		})
 	}
 	seane4.addChild(typing);
@@ -188,23 +188,23 @@ seane4.set (function(){
 ////////////////////////////////////////
 var seane5 = new Seane();
 seane5.addClass("seane5")
-seane5.added=function(){
+seane5.added = function () {
 	$(".ob2 span").html(Rec.totalPoint + "点");
-	var twurl = "http://twitter.com/?status=";
-	twurl +=encodeURIComponent("【"+ document.title +"】今回の採点結果は、"+Rec.totalPoint+"点です。#html_typing_game "+"https://github.com/KentaIshikawa/typingGame");
-	
-	$(".btnTw").html("<a href="+twurl+" target='_blank'><span>tweeterでつぶやく</span></a>");
+	var twurl = "https://twitter.com/intent/tweet?status=";
+	twurl += encodeURIComponent("【" + document.title + "】今回の採点結果は、" + Rec.totalPoint + "点です。#html_typing_game " + "https://github.com/KentaIshikawa/typingGame");
+
+	$(".btnTw").html("<a href=" + twurl + " target='_blank'><span>tweeterでつぶやく</span></a>");
 }
 
 
-seane5.removed=function(){
+seane5.removed = function () {
 	Rec.totalPoint = 0;
 }
 
-seane5.set(function(){
+seane5.set(function () {
 	var btn = new Btn();
 	btn.html = "<span>タイトルへ戻る</span>";
-	btn.addEvent(btn.id,"click",function(){
+	btn.addEvent(btn.id, "click", function () {
 		Stage.removeStage(seane5);
 		Stage.addStage(seane1);
 	});
@@ -214,22 +214,22 @@ seane5.set(function(){
 	var btnRe = new Btn();
 	btnRe.addClass("btnRe");
 	btnRe.html = "<span>リトライ</span>"
-	btnRe.addEvent(btnRe.id,"click",function(){
+	btnRe.addEvent(btnRe.id, "click", function () {
 		Stage.removeStage(seane5);
 		Stage.addStage(seane3);
 	});
-	seane5.addChild	(btnRe);
-	
+	seane5.addChild(btnRe);
+
 	var btnTw = new Btn();
 	btnTw.addClass("btnTw");
-	seane5.addChild	(btnTw);
+	seane5.addChild(btnTw);
 
 
 	var ob1 = new Ob();
 	ob1.addClass("ob1");
 	ob1.html = "採点結果";
 	seane5.addChild(ob1);
-	
+
 	var ob2 = new Ob();
 	ob2.addClass("ob2");
 	ob2.html = "今回の点数は、<span></span>です。";
